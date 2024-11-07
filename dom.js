b37e4607721870b96caae7c42b930737fd4ba59c
pavani pulicharla <pavani513>
2024-11-07 17:48:20 +0530

    files added

---
+++ dom.js
@@ -0,0 +1,16 @@
+let a = document.getElementById("btn")
+a.onclick = function j() {
+    a.textContent = "Hello.!! Smile Please"
+    a.style.backgroundColor = "red"
+    a.style.fontSize = "60px"
+    a.style.borderRadius = "50%"
+}
+function p()
+{
+let b=document.getElementById("box");
+    b.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRKw-lhOB271eZd8y5faIo-j70Nw1A_5hLSA&s" width=250px style="border-radius:30px"/>
+    <img src="https://static.tnn.in/thumb/msid-112881107,thumbsize-37604,width-1280,height-720,resizemode-75/112881107.jpg?quality=100" width=300px style="border-radius:100px"/>
+    <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202407/nanis-2nd-look-from-saripodhaa-sanivaaram-out-041920157-16x9_0.jpg?VersionId=ZNO0n2xNN5jfPZMplU6ZQLojPvjWTmxf&size=690:388" width=300px style="border-radius:50%"/>`
+
+}
+
